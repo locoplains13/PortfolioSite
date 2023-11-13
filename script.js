fabbafc1fa51cae37cf6cb817c606c40b90c5bb5
Angel Roberto Andrade <alroc1234@hotmail.com>
2023-11-13 04:15:52 -0600

    testing for vercel analytics

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ import { Sphere } from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { inject } from "@vercel/analytics";
 
+inject();
 const scene = new THREE.Scene();
 scene.background = null;
 const camera = new THREE.PerspectiveCamera(
